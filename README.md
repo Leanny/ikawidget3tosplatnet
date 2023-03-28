c26789d65fcee53234ab9384304f3bc5e7aa6352
Leanny <tales-of@live.de>
2023-03-28 22:59:16 +0200

    Initial version

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# ikawidget3tosplatnet
-Converts ikawidget3 databases to raw splatnet output
+# ikawidget3 to splatnet
+Converts ikawidget3 databases to raw splatnet output.
+
+## Requirements
+* python3
+* [cblite](https://github.com/couchbaselabs/couchbase-mobile-tools/releases/tag/cblite-3.0.0EE-alpha)
+
+## How To
+* Export your database from ikawidget3
+* Copy the database over to your PC
+* Put cblite and the script in the same folder
+* Use command `python3 [name_of_database].ikax3` to generate a `converted_[timestamp].json` file. This file can be uploaded at [the profile page of the splatoon 3 seedhecker](https://leanny.github.io/splat3seedchecker/#/profile) as `Raw Splatnet` File.
---
+++ convert.py
@@ -0,0 +1,32 @@
+import sys
+import json
+import subprocess
+import zipfile
+import glob
+import shutil
+import time
+
+def toCoop(data): 
+    return [{"data": {"coopHistoryDetail": json.loads(d)}} for d in data]
+
+def toVersus(data):
+    res = []
+    for d in data:
+        conv = json.loads(d)
+        if "myTeam" in conv and "players" in conv["myTeam"]:
+            res.append({"data": {"vsHistoryDetail": conv}}) 
+    return res
+
+if __name__ == "__main__":
+    filename = sys.argv[1]
+    shutil.rmtree("tmp", ignore_errors=True)
+    with zipfile.ZipFile(filename, 'r') as zip_ref:
+        zip_ref.extractall("tmp")
+    dirs = glob.glob("tmp/*/*.cblite2")
+    full_res = []
+    for d in dirs:
+        data_s = toCoop if "coopResult" in d else toVersus
+        out = subprocess.check_output(["cblite", "cat", "--raw", d, "*"], encoding="utf8")
+        full_res.extend(data_s(out.splitlines()))
+
+    json.dump(full_res, open(f"converted_{int(time.time())}.json", "w"))
